5a3dc7db01ad9a87664d0bad460bce8b2a5f3583
SamuelMUniz9 <msamuelmuniz@gmail.com>
2025-04-23 22:28:58 -0300

    codigo finalizado

--- Desktop/Samuel/eclipse/Aleatorio/DesafioControleFluxo/src/Contador/ParametrosInvalidosException.java
+++ Desktop/Samuel/eclipse/Aleatorio/DesafioControleFluxo/src/Contador/ParametrosInvalidosException.java
@@ -1,5 +1,9 @@
 package Contador;
 
-public class ParametrosInvalidosException {
-
+public class ParametrosInvalidosException extends Exception{
+	  public ParametrosInvalidosException (String mensagem) {
+	       super(mensagem);
+	}
 }
+
+//entender aonde faço a logica aqui ou la no main, a logica do if else pra contagem, to achando que é la 
--- Desktop/Samuel/eclipse/Aleatorio/DesafioControleFluxo/src/Contador/Principal.java
+++ Desktop/Samuel/eclipse/Aleatorio/DesafioControleFluxo/src/Contador/Principal.java
@@ -1,10 +1,45 @@
 package Contador;
 
+import java.util.Scanner;
+
 public class Principal {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		terminal.close();
+		
+		//ele cria os parametros ja na hora de pegar as informações com o scanner
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			  System.out.println("Erro: " + e.getMessage());
+			
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if( parametroUm >= parametroDois) {
+			throw new ParametrosInvalidosException("O segundo parametro tem que ser maior que o primeiro");
+			
+		//por que nao precisa de return? 
+		} 
+		
+	
+		
+		int resultado = (parametroDois - parametroUm);  
+			for( int numero = 1; numero <= resultado; numero++)
+			System.out.println("Imprimindo o número" + numero); 
+			
+	
 	}
 
 }
